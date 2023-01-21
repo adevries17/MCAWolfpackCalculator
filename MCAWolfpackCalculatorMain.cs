87cab13601c321b94345afaa172294714da5c801
adevries17 <andrew@andrewdevries.net>
2023-01-21 11:51:41 -0600

    initial wpf conversion and ui design

--- MCAWolfpackCalculatorMain.cs
+++ MCAWolfpackCalculatorMain.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace MCAWolfpackCalculator {
-    static class MCAWolfpackCalculatorMain {
+namespace MCAWolfpackCalculator
+{
+    static class MCAWolfpackCalculatorMain
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main()
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MCAWolfpackCalculatorForm());
