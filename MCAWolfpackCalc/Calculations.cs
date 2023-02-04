7498636173e336c1f8f58b13be1166355b96b4b7
adevries17 <andrew@andrewdevries.net>
2023-02-03 21:54:12 -0600

    round to tenths place

--- MCAWolfpackCalc/Calculations.cs
+++ MCAWolfpackCalc/Calculations.cs
@@ -1,15 +1,17 @@
-﻿namespace MCAWolfpackCalc {
+﻿using System;
+
+namespace MCAWolfpackCalc {
     internal class Calculations {
         public static double CalculateTargetRange(double mastheight, double mils, bool? zoomed) {
             return zoomed switch {
-                true => mastheight / mils * 4 * 0.9167,
-                false => mastheight / mils * 0.9167,
+                true => Math.Round(mastheight / mils * 4 * 0.9167, 1),
+                false => Math.Round(mastheight / mils * 0.9167, 1),
                 _ => 0,
             };
         }
 
         public static double CalculateTargetSpeed(double shiplength, double seconds) {
-            return shiplength / seconds * 1.944;
+            return Math.Round(shiplength / seconds * 1.944, 1);
         }
     }
 }
